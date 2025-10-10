30440b65653976fce04365cbc8a503be8a27f1b8
jftrades <ferdijofer@icloud.com>
2025-10-10 12:15:46 +0200

    column content (app.post) first integration

--- correlation_backend/logic/read_data.py
+++ correlation_backend/logic/read_data.py
@@ -1,18 +1,54 @@
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-import correlation_backend.logic.calc_analysis
 
 
-def read_parquet_files(data_folder='data/BTCUSDT-PERP.BINANCE_LUNAR'):
-    data_frames = {}
-    for filename in os.listdir(data_folder):
-        if filename.endswith('.parquet'):
-            file_path = os.path.join(data_folder, filename)
-            df = pd.read_parquet(file_path)
-            data_frames[filename] = df
-    return data_frames
+def get_target_data_by_instrument(instrument: str, data_type: str, data_column: str):
+    data_folder = "data"
+    instrument = instrument.lower()
+    data_type = data_type.lower()
 
-def get_specific_column(df: pd.DataFrame, col_index: int):
-    return df.iloc[:, col_index]
+    for foldername in os.listdir(data_folder):
+        if instrument in foldername.lower():
+            folder_path = os.path.join(data_folder, foldername)
+            for subfolder in os.listdir(folder_path):
+                if data_type in subfolder.lower():
+                    exact_folder_path = os.path.join(folder_path, subfolder)
+                    
+                    filename = os.listdir(exact_folder_path)[0]
 
+                    df = pd.read_parquet(os.path.join(exact_folder_path, filename))
+                    if data_column in df.columns:
+                        return df[data_column]
+    
+    return None
+
+
+def get_comparison_data_by_instrument(comparison_instruments: list[str], comparison_datatypes: list[str], comparison_data_columns: list[str]):
+    data_folder = "data"
+    results = []
+    
+    for instrument, data_type, data_column in zip(comparison_instruments, comparison_datatypes, comparison_data_columns):
+        instrument = instrument.lower()
+        data_type = data_type.lower()
+        
+        for foldername in os.listdir(data_folder):
+            if instrument in foldername.lower():
+                folder_path = os.path.join(data_folder, foldername)
+                for subfolder in os.listdir(folder_path):
+                    if data_type in subfolder.lower():
+                        exact_folder_path = os.path.join(folder_path, subfolder)
+                        
+                        filename = os.listdir(exact_folder_path)[0]
+
+                        df = pd.read_parquet(os.path.join(exact_folder_path, filename))
+                        if data_column in df.columns:
+                            results.append(df[data_column])
+                            break
+                else:
+                    continue
+                break
+        else:
+            results.append(None)
+    
+    return results
--- correlation_backend/main.py
+++ correlation_backend/main.py
@@ -1,31 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
-from correlation_backend.logic.read_data import read_parquet_files, get_specific_column
 from correlation_backend.logic.calc_analysis import pearson_analysis, spearman_analysis, kendalltau_analysis, linregress_analysis, theilslopes_analysis
 from fastapi.middleware.cors import CORSMiddleware
+from correlation_backend.logic.read_data import get_target_data_by_instrument, get_comparison_data_by_instrument
 
 pearson_values = None
 
 SPECIFIC_COLUMN_INDEX_1 = 4
 SPECIFIC_COLUMN_INDEX_2 = 5
 
-dfs = read_parquet_files()
-filename, df = next(iter(dfs.items()))
-specific_column_1 = get_specific_column(df, SPECIFIC_COLUMN_INDEX_1)
-specific_column_2 = get_specific_column(df, SPECIFIC_COLUMN_INDEX_2)
-print(specific_column_1)
-print(specific_column_2)
+# dfs = read_parquet_files()
+# filename, df = next(iter(dfs.items()))
+# specific_column_1 = get_specific_column(df, SPECIFIC_COLUMN_INDEX_1)
+# specific_column_2 = get_specific_column(df, SPECIFIC_COLUMN_INDEX_2)
+# print(specific_column_1)
+# print(specific_column_2)
 
-pearson_values = pearson_analysis(specific_column_1, specific_column_2)
-print(pearson_values)
+# pearson_values = pearson_analysis(specific_column_1, specific_column_2)
+# print(pearson_values)
 
-spearman_values = spearman_analysis(specific_column_1, specific_column_2)
-print(spearman_values)
+# spearman_values = spearman_analysis(specific_column_1, specific_column_2)
+# print(spearman_values)
 
-kendalltau_values = kendalltau_analysis(specific_column_1, specific_column_2)
-print(kendalltau_values)
+# kendalltau_values = kendalltau_analysis(specific_column_1, specific_column_2)
+# print(kendalltau_values)
 
 app = FastAPI()
 
@@ -37,14 +38,38 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-@app.get("/")
-async def root():
-    return {"pearson_values": pearson_values, "spearman_values": spearman_values, "kendalltau_values": kendalltau_values}
-
-@app.get("/files")
-async def get_files():
-    return {"files": list(dfs.keys())}
+# @app.get("/files")
+# async def get_files():
+#     return {"files": list(dfs.keys())}
 
 @app.get("/analysismethods")
 async def get_analysis_methods():
     return {"analysis_methods": ["pearson", "spearman", "kendalltau"]}
+
+class FindFilePath(BaseModel):
+    target_instrument: str 
+    target_datatype: str
+    target_data_column: str
+
+    comparison_instruments: list[str]
+    comparison_datatypes: list[str]
+    comparison_data_columns: list[str]
+
+    methods: list[str]
+
+@app.post("/filepath")
+async def analyze(request: FindFilePath):
+    target_instrument = request.target_instrument
+    target_data_type = request.target_datatype
+    target_data_column = request.target_data_column
+    
+    comparison_instruments = request.comparison_instruments
+    comparison_data_types = request.comparison_datatypes
+    comparison_data_columns = request.comparison_data_columns
+    
+    methods = request.methods
+
+    target_column_content = get_target_data_by_instrument(target_instrument, target_data_type, target_data_column)
+    comparison_column_contents = get_comparison_data_by_instrument(comparison_instruments, comparison_data_types, comparison_data_columns)
+
+    return {"target_column_content": target_column_content, "comparison_column_contents": comparison_column_contents}
