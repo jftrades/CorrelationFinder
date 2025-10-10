68c7e43ba6f1063336dff5b6c68a9198d36b49bf
LeopoldLabs <mail@leoj.de>
2025-10-10 14:53:52 +0200

    😛 😛

--- correlation_web/src/App.tsx
+++ correlation_web/src/App.tsx
@@ -8,6 +8,8 @@ import {
 } from "@/components/ui/select";
 import { Field, FieldLabel } from './components/ui/field';
 import { useState } from 'react';
+import { DropdownMenu, DropdownMenuCheckboxItem, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 const instruments = {
   "btc": "Bitcoin",
@@ -42,6 +44,10 @@ export default function App() {
   const [targetDatatype, setTargetDatatype] = useState<string>("");
   const [targetDataColumn, setTargetDataColumn] = useState<string>("");
 
+  const [comparisonInstruments, setComparisonInstruments] = useState<string[]>([]);
+  const [comparisonDatatypes, setComparisonDatatypes] = useState<string[]>([]);
+  const [comparisonDataColumns, setComparisonDataColumns] = useState<string[]>([]);
+
   const { data, isLoading, error } = useQuery({
     queryKey: ["availableDataColumns", targetInstrument, targetDatatype],
     queryFn: () => getAvailableDataColumns(targetInstrument, targetDatatype),
@@ -51,15 +57,24 @@ export default function App() {
 
   let availableDataColumns = data ?? [];
 
+  function handleComparisonInstrumentClick(instrument: string) {
+    if (comparisonInstruments.includes(instrument)) {
+      setComparisonInstruments(comparisonInstruments.filter((i) => i !== instrument));
+    } else {
+      setComparisonInstruments([...comparisonInstruments, instrument]);
+    }
+  }
+
   return (
     <div className="flex justify-center p-8 min-h-screen w-full">
-      <main className="flex flex-col gap-8 max-w-3xl w-full items-start">
+      <main className="flex flex-col gap-8 max-w-5xl w-full items-start">
         <h1 className="text-4xl font-bold">Correlation Analysis</h1>
-        <div className="flex flex-col gap-6">
+
+        <div className="grid grid-cols-3 gap-6 w-full">
           <Field>
             <FieldLabel>Target Instrument</FieldLabel>
             <Select value={targetInstrument} onValueChange={setTargetInstrument}>
-              <SelectTrigger>
+              <SelectTrigger className="w-full">
                 <SelectValue placeholder="Select a target instrument" />
               </SelectTrigger>
               <SelectContent>
@@ -72,7 +87,7 @@ export default function App() {
           <Field>
             <FieldLabel>Target Datatype</FieldLabel>
             <Select value={targetDatatype} onValueChange={setTargetDatatype}>
-              <SelectTrigger>
+              <SelectTrigger className="w-full">
                 <SelectValue placeholder="Select a target datatype" />
               </SelectTrigger>
               <SelectContent>
@@ -85,7 +100,7 @@ export default function App() {
           <Field>
             <FieldLabel>Target Data Column</FieldLabel>
             <Select value={targetDataColumn} onValueChange={setTargetDataColumn} disabled={isLoading || !!error || availableDataColumns.length === 0}>
-              <SelectTrigger>
+              <SelectTrigger className="w-full">
                 <SelectValue placeholder={isLoading ? "Loading..." : !!error ? "Error loading data columns" : availableDataColumns.length === 0 ? "No data columns available" : "Select a target data column"} />
               </SelectTrigger>
               <SelectContent>
@@ -94,6 +109,24 @@ export default function App() {
                 ))}
               </SelectContent>
             </Select>
+          </Field>
+          <Field>
+            <FieldLabel>Comparison Instruments</FieldLabel>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="outline" className="font-normal">{comparisonInstruments.length > 0 ? comparisonInstruments.join(", ") : "Select comparison instruments"}</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56">
+                {Object.entries(instruments).map(([key, value]) => (
+                  <DropdownMenuCheckboxItem
+                    key={key}
+                    checked={comparisonInstruments.includes(key)}
+                    onClick={() => handleComparisonInstrumentClick(key)}>
+                    {value}
+                  </DropdownMenuCheckboxItem>
+                ))}
+              </DropdownMenuContent>
+            </DropdownMenu>
           </Field>
         </div>
       </main>
